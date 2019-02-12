c796429b1ece88f552858d097b18dc9a4f4819ae
Winnie Fong <winniefong21@gmail.com>
2019-02-12 00:06:02 -0800

    Able to render restaurant address from db to client

--- client/src/components/ResAddress.jsx
+++ client/src/components/ResAddress.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
+import $ from 'jquery';
 
 import { IconSize, SpanStyles, SpanHover } from './Styles';
 
 class ResAddress extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      address: '',
+    };
+
+    this.getAddress = this.getAddress.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAddress();
+  }
+
+  getAddress() {
+    const getData = (callback) => {
+      $.get({
+        url: 'http://localhost:3030/izakayaInfo',
+        success: data => callback(null, data.address),
+        error: err => callback(err),
+      });
+    };
+
+    getData((err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      this.setState({
+        address: data,
+      });
+    });
+  }
 
   render() {
     return (
       <div className="business-address">
         <IconSize src="https://s3-us-west-1.amazonaws.com/zagat-screenshots/address_icon.jpg"></IconSize>
         <SpanStyles>
-          <SpanHover>1500 Irving St, San Francisco, CA 94122, USA</SpanHover>
+          <SpanHover>{this.state.address}</SpanHover>
         </SpanStyles>
       </div>
     );
